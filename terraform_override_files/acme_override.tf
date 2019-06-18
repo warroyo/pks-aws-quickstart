e87d2fbe17bd7919fc66efe4b840152f003b7d36
warroyo <warroyo7199008@gmail.com>
2019-06-18 12:39:19 -0600

    update version

--- terraform_override_files/acme_override.tf
+++ terraform_override_files/acme_override.tf
@@ -7,7 +7,7 @@ variable "email" {
 
 provider "acme" {
   server_url = "https://acme-v02.api.letsencrypt.org/directory"
-  # version = "~> 1.3.4"
+  version = "~> 1.2.1"
 }
 
 resource "tls_private_key" "pks_private_key" {

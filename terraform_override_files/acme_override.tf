fd5a86b16ae8a25faf8b47ccf1d70ad97517fdc5
warroyo <warroyo7199008@gmail.com>
2019-06-18 12:00:09 -0600

    add certs back in

--- terraform_override_files/acme_override.tf
+++ terraform_override_files/acme_override.tf
@@ -31,76 +31,76 @@ resource "null_resource" "dns-propagation-wait" {
 
 ## CF certificate
 
-# resource "acme_certificate" "pks-certificate" {
-#   account_key_pem           = "${acme_registration.reg.account_key_pem}"
-#   common_name               = "${module.pks.domain}"
-#   depends_on                = ["aws_route53_record.nameserver","null_resource.dns-propagation-wait"]
+resource "acme_certificate" "pks-certificate" {
+  account_key_pem           = "${acme_registration.reg.account_key_pem}"
+  common_name               = "${module.pks.domain}"
+  depends_on                = ["aws_route53_record.nameserver","null_resource.dns-propagation-wait"]
   
 
-#   dns_challenge {
-#     provider                  = "route53"
-#     config {
-#       AWS_ACCESS_KEY_ID     = "${var.access_key}"
-#       AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
-#       AWS_HOSTED_ZONE_ID    = "${module.infra.zone_id}"
-#     }
-#   }
-# }
+  dns_challenge {
+    provider                  = "route53"
+    config {
+      AWS_ACCESS_KEY_ID     = "${var.access_key}"
+      AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
+      AWS_HOSTED_ZONE_ID    = "${module.infra.zone_id}"
+    }
+  }
+}
 
-# output "ssl_cert" {
-#   sensitive = true
-#   value     = <<EOF
-# ${acme_certificate.pks-certificate.certificate_pem}
-# ${acme_certificate.pks-certificate.issuer_pem}
-# EOF
-# }
+output "ssl_cert" {
+  sensitive = true
+  value     = <<EOF
+${acme_certificate.pks-certificate.certificate_pem}
+${acme_certificate.pks-certificate.issuer_pem}
+EOF
+}
 
-# output "ssl_private_key" {
-#   sensitive = true
-#   value     = "${acme_certificate.pks-certificate.private_key_pem}"
-# }
-
-
-# resource "aws_acm_certificate" "certificate" {
-#   private_key      = "${acme_certificate.pks-certificate.private_key_pem}"
-#   certificate_body = "${acme_certificate.pks-certificate.certificate_pem}"
-#   certificate_chain  = "${acme_certificate.pks-certificate.issuer_pem}"
-#   tags = {
-#     Name = "${var.env_name}-pks-lbcert"
-#   } 
-#   lifecycle = {
-#     create_before_destroy = true
-#   }
-# }
+output "ssl_private_key" {
+  sensitive = true
+  value     = "${acme_certificate.pks-certificate.private_key_pem}"
+}
 
 
+resource "aws_acm_certificate" "certificate" {
+  private_key      = "${acme_certificate.pks-certificate.private_key_pem}"
+  certificate_body = "${acme_certificate.pks-certificate.certificate_pem}"
+  certificate_chain  = "${acme_certificate.pks-certificate.issuer_pem}"
+  tags = {
+    Name = "${var.env_name}-pks-lbcert"
+  } 
+  lifecycle = {
+    create_before_destroy = true
+  }
+}
 
-# ### Opsman
 
-# resource "acme_certificate" "opsman-certificate" {
-#   account_key_pem           = "${acme_registration.reg.account_key_pem}"
-#   common_name               = "${module.ops_manager.dns}"
-#   depends_on                = ["aws_route53_record.nameserver","null_resource.dns-propagation-wait"]
 
-#   dns_challenge {
-#     provider                  = "route53"
-#      config {
-#       AWS_ACCESS_KEY_ID     = "${var.access_key}"
-#       AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
-#       AWS_HOSTED_ZONE_ID    = "${module.infra.zone_id}"
-#     }
-#   }
-# }
+### Opsman
 
-# output "opsman_ssl_cert" {
-#   sensitive = true
-#   value     = <<EOF
-# ${acme_certificate.opsman-certificate.certificate_pem}
-# ${acme_certificate.opsman-certificate.issuer_pem}
-# EOF
-# }
+resource "acme_certificate" "opsman-certificate" {
+  account_key_pem           = "${acme_registration.reg.account_key_pem}"
+  common_name               = "${module.ops_manager.dns}"
+  depends_on                = ["aws_route53_record.nameserver","null_resource.dns-propagation-wait"]
 
-# output "opsman_ssl_private_key" {
-#   sensitive = true
-#   value     = "${acme_certificate.opsman-certificate.private_key_pem}"
-# }
+  dns_challenge {
+    provider                  = "route53"
+     config {
+      AWS_ACCESS_KEY_ID     = "${var.access_key}"
+      AWS_SECRET_ACCESS_KEY = "${var.secret_key}"
+      AWS_HOSTED_ZONE_ID    = "${module.infra.zone_id}"
+    }
+  }
+}
+
+output "opsman_ssl_cert" {
+  sensitive = true
+  value     = <<EOF
+${acme_certificate.opsman-certificate.certificate_pem}
+${acme_certificate.opsman-certificate.issuer_pem}
+EOF
+}
+
+output "opsman_ssl_private_key" {
+  sensitive = true
+  value     = "${acme_certificate.opsman-certificate.private_key_pem}"
+}
